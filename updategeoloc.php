020d7eaf418d0570be86671c84681693b18d73f6
DrPins <phuveteau@gmail.com>
2018-04-29 19:51:55 +0200

    maj

--- updategeoloc.php
+++ updategeoloc.php
@@ -8,17 +8,17 @@ $lat = $_POST['lat'];
 $lng = $_POST['lng'];
 
 
-if(!isset($lastname) || !isset($coordinates)){
+if(!isset($lastname) || !isset($lat)|| !isset($lng)){
  echo "";
  header('http/1.1 403 Forbiden');
- retun;
+ return;
 }
 
 //$id = $s->id;
 
 // si l'intervention fini, on ajouter en base le temps et le rapport et on passe pending à 1
 
-$requete = $db->prepare("INSERT INTO positions (id_employee, date_position, lat, lng) VALUES ((select id from employees where lastname = '$lastname'),GETDATE(), '$lat', '$lng' )");
+$requete = $db->prepare("INSERT INTO coordinates (id_employee, date_position, lat, lng) VALUES ((select id from employees where lastname = '$lastname'),GETDATE(), '$lat', '$lng' )");
 $requete->execute();
 
 ?>

0db1dee5f479e955d4779c4cbeff6ac1f67caf16
DrPins <phuveteau@gmail.com>
2018-05-31 10:34:16 +0200

    test

--- update.php
+++ update.php
@@ -6,7 +6,7 @@ require_once('common.php');
 $id_inter = $_POST['id_intervention'];
 $report = $_POST['report'];
 $duration = $_POST['duration'];
-$distance = $_POST['distance'];
+//$distance = $_POST['distance'];
 if(!isset($id_inter) || $report = "" || $duration = "00:00:00.0000000"){
  echo "";
  header('http/1.1 403 Forbiden');
@@ -15,7 +15,7 @@ if(!isset($id_inter) || $report = "" || $duration = "00:00:00.0000000"){
 
 // si l'intervention fini, on ajouter en base le temps et le rapport et on passe pending à 1
 
-$requete = $db->prepare("UPDATE interventions SET pending = 1, report ='$report', duration='$duration', distance='$distance'  where id = '$id_inter'");
+$requete = $db->prepare("UPDATE interventions SET pending = 1, report ='$report', duration='$duration'  where id = '$id_inter'");
 $requete->execute();
 
 ?>
